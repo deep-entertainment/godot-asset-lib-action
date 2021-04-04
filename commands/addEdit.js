a4e9c19edaf9599f32fe979770802faeb68ef532
Dennis Ploeger <develop@dieploegers.de>
2021-04-04 11:11:39 +0200

    fix: Moved back to ncc and axios

--- commands/addEdit.js
+++ commands/addEdit.js
@@ -1,26 +1,21 @@
-import { Command } from './command'
+import { Command } from "./command"
 
-const core = require('@actions/core')
-const github = require('@actions/github');
-const fs = require('fs').promises
-const superagent = require('superagent')
-
+const core = require("@actions/core")
+const github = require("@actions/github")
+const fs = require("fs").promises
+const axios = require("axios").default
 
 export default class extends Command {
-    async do () {
-        const templateContent = await fs.readFile(
-            core.getInput('assetTemplate'),
-            {
-                encoding: 'utf-8'
-            }
-        )
-        const baseUrl = core.getInput('baseUrl')
-        const assetId = core.getInput('assetId')
-        const template = Handlebars.compile(templateContent)
-        const assetEdit = JSON.parse(template(github.context.payload))
-        assetEdit.token = token
-        const res = await superagent.post(`${baseUrl}/asset/${assetId}`)
-            .send(assetEdit)
-        core.setOutput('id', res.id)
-    }
+  async do() {
+    const templateContent = await fs.readFile(core.getInput("assetTemplate"), {
+      encoding: "utf-8",
+    })
+    const baseUrl = core.getInput("baseUrl")
+    const assetId = core.getInput("assetId")
+    const template = Handlebars.compile(templateContent)
+    const assetEdit = JSON.parse(template(github.context.payload))
+    assetEdit.token = token
+    const res = await axios.post(`${baseUrl}/asset/${assetId}`, assetEdit)
+    core.setOutput("id", res.data.id)
+  }
 }
--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const superagent = require('superagent')
-
-(async () => {
-    try {
-        const action = core.getInput('action')
-        let commandClass
-        switch (action) {
-            case 'addEdit':
-                commandClass = require('commands/addEdit')
-                break
-            default:
-                throw new Error(`Unknown action ${action}`)
-        }
-        const baseUrl = core.getInput('baseUrl')
-        const username = core.getInput('username')
-        const password = core.getInput('password')
-        console.log(`Logging in ${username}`)
-
-        const res = await superagent.post(`${baseUrl}/login`)
-            .send({
-                "username": username,
-                "password": password
-            })
-
-        const token = res.body.token
-        const command = new commandClass(token)
-        await command.do()
-        await superagent.post(`${baseUrl}/logout`)
-            .send({
-                "token": token
-            })
-    } catch (error) {
-        core.setFailed(error.message);
+const core = require("@actions/core")
+const github = require("@actions/github")
+const axios = require("axios").default(async () => {
+  try {
+    const action = core.getInput("action")
+    let commandClass
+    switch (action) {
+      case "addEdit":
+        commandClass = require("commands/addEdit")
+        break
+      default:
+        throw new Error(`Unknown action ${action}`)
     }
-})();
+    const baseUrl = core.getInput("baseUrl")
+    const username = core.getInput("username")
+    const password = core.getInput("password")
+    console.log(`Logging in ${username}`)
+
+    const res = await axios.post(`${baseUrl}/login`, {
+      username: username,
+      password: password,
+    })
+
+    const token = res.data.token
+    const command = new commandClass(token)
+    await command.do()
+    await axis.post(`${baseUrl}/logout`, {
+      token: token,
+    })
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+})()
