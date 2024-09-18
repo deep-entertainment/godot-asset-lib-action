b86775d4387e06b3bb1a411b6d14e6bec98b301f
Nathan Flurry <developer@nathanflurry.com>
2024-09-18 14:51:05 -0700

    feat: accept custom working dir

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 const axios = require('axios').default
+const path = require('path')
 
 ;(async () => {
   try {
@@ -16,6 +17,8 @@ const axios = require('axios').default
     const baseUrl = core.getInput('baseUrl')
     const username = core.getInput('username')
     const password = core.getInput('password')
+    const directory = core.getInput('directory')
+    const assetTemplatePath = path.resolve(process.cwd(), core.getInput('assetTemplate'))
     console.log(`Logging in ${username}`)
 
     const res = await axios.post(`${baseUrl}/login`, {
@@ -25,8 +28,8 @@ const axios = require('axios').default
 
     const token = res.data.token
 
-    console.log(`Starting command for action ${action}`)
-    const command = new commandClass(token)
+    console.log(`Starting command for action ${action} in directory ${directory}`)
+    const command = new commandClass(token, assetTemplatePath)
     await command.do()
 
     console.log('Logging out of asset lib')
